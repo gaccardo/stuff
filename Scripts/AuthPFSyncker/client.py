ad369df7e91eebd56a5db7a7bbc14cd373152bec
gaccardo <gaccardo@gmail.com>
2013-04-08 18:01:46 -0300

    Update client.py
    sdf

--- Scripts/AuthPFSyncker/client.py
+++ Scripts/AuthPFSyncker/client.py
@@ -8,8 +8,7 @@ Client for use with authpf server
 """
 
 __version__   = "0.1"
-__author__    = "Guido Accardo <gaccardo@coresecurity.com>"
-__copyright__ = "Core Security Technologies S.A."
+__author__    = "Guido Accardo <gaccardo@gmail.com>"
 
 if sys.argv[1] == "-U" or sys.argv[1] == "--userexists":
         print "OK"
