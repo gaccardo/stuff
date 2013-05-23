6ee4da24e786347c77c064d9ef6f4a6d6f6ba051
Guido Accardo <gaccardo@gmail.com>
2013-05-23 10:49:45 -0300

    Decorator testing

---
+++ Modules/Decorators.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import traceback
+import sys 
+
+from MainException import *
+
+AUTHENTICATED = False
+
+def auth(f):
+   def inner(*args, **kwargs):
+      if AUTHENTICATED:
+         f(*args, **kwargs)
+      else:
+         tb = traceback.format_stack()
+         raise UserNotAuthenticated('[PERMISSION DENIED]: User is not authenticated',
+                                    f.__name__, tb) 
+
+   return inner
