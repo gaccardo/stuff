fd0e3b7f56c695da778e544d07a412211f222408
Guido Accardo <gaccardo@gmail.com>
2013-10-07 09:45:01 -0300

    New estructure of ssher in order to prepare it to be a Python EGG

---
+++ Scripts/ssher/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='SSHer',
+    version='1.0',
+    description='Multi ssh connection launcher',
+    author='Guido Accardo',
+    author_email='gaccardo@gmail.com',
+    url='http://wiki.secondlife.com/wiki/Eventlet',
+    packages=['ssher'])
